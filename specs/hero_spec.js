b07cd507e8c31962470779141ac24db9916149ac
Peter McCready <petermccready2@gmail.com>
2017-11-30 17:07:21 +0000

    Adds hero eat functionality

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,14 @@
 var assert = require('assert');
 var Hero = require('../hero.js');
 var Task = require('../task.js');
+var Food = require('../food.js');
 
 describe("Hero", function(){
 
   beforeEach(function(){
     hero = new Hero("Hayley", 3000, "Cheese");
     task = new Task("Complete task tests", 3, 1, 10);
+    food1 = new Food("Apple", 5);
   });
 
   it('should have a name', function(){
@@ -32,6 +34,15 @@ describe("Hero", function(){
   it('should be able to add a task to task list', function(){
     hero.addTask(task);
     assert.strictEqual(hero.tasks.length, 1);
+  });
+
+  it('should be able to eat food and their health go up by replenishment value', function(){
+    // arrange
+    var expected = hero.health + food1.replenValue;
+    // act
+    hero.eat(food1);
+    // assert
+    assert.strictEqual(hero.health, expected);
   });
 
 });
